19051c1d2d382b522444ca288a6f57889d116dfe
Owais Dashti <owaisdashti@outlook.com>
2024-11-15 00:28:41 +0100

    little bit of styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,14 @@
 import { SiIfood } from "react-icons/si";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { Link } from "react-router-dom";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 const Header = () => {
   return (
@@ -13,9 +21,33 @@ const Header = () => {
         </Link>
       </div>
       <div>
-        <div className="hover:bg-gray-50 p-3 hover:rounded-full cursor-pointer">
-          <RxHamburgerMenu className="" size={26} />
-        </div>
+        <Dialog>
+          <DialogTrigger>
+            <div className="hover:bg-gray-50 p-3 hover:rounded-full cursor-pointer">
+              <RxHamburgerMenu className="" size={26} />
+            </div>
+          </DialogTrigger>
+          <DialogContent className="h-full md:h-fit">
+            <DialogHeader>
+              <DialogTitle className="text-2xl font-semibold">My account</DialogTitle>
+
+              {/* sign in and create account buttons */}
+              <div className="flex gap-2 w-full items-center justify-center">
+                <Link to="/signin" className="text-lg font-semibold text-gray-800 hover:text-gray-900">
+                  Sign in
+                </Link>
+                <Link to="/signup" className="text-lg font-semibold text-gray-800 hover:text-gray-900">
+                  Create account
+                </Link>
+              </div>
+
+              <DialogDescription>
+                This action cannot be undone. This will permanently delete your account and remove your data from our
+                servers.
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
       </div>
     </div>
   );
